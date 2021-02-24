06ff8cbd31f9af5271943f8c1cf5ce933f8079a3
Daniel Hum <dan@oneuphero.com>
2021-02-24 14:47:25 +0800

    feat: Konami key combo easter egg
      - wip commit
      - add useKonamiCode hook to detect key combo (todo: refactor)
      - add GlobalKeyComboDetector component to App

---
+++ src/views/GlobalKeyComboDetector.tsx
@@ -0,0 +1,14 @@
+import useKonamiCode from 'hooks/useKonamiCode'
+
+const GlobalKeyComboDetector = () => {
+  const completeComboCallback = () => {
+    // TODO: show pancake-flake effect
+    alert('C-C-COMBO!!')
+  }
+
+  useKonamiCode(completeComboCallback)
+
+  return null
+}
+
+export default GlobalKeyComboDetector
